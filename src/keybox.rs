1f92dbd3ddad70c6e8e8cd65f2be87a49c459f42
ArkDra <arkdra@qq.com>
2026-04-05 22:00:56 +0800

    refactor: optimize apply_keystream method

--- src/keybox.rs
+++ src/keybox.rs
@@ -1,5 +1,7 @@
 ﻿use anyhow::{bail, Result};
-use rayon::{iter::ParallelIterator, slice::ParallelSliceMut};
+use rayon::prelude::*;
+
+const CHUNK_SIZE: usize = 16384;
 
 pub struct KeyBox {
     box_data: [u8; 256],
@@ -28,19 +30,20 @@ impl KeyBox {
         Ok(KeyBox { box_data: key_box })
     }
 
-    pub fn apply_keystream(&self, data: Vec<u8>) -> Vec<u8> {
-        let chunk_size = 16384;
-
-        let mut output = data.clone();
-        output.par_chunks_mut(chunk_size).for_each(|chunk| {
-            chunk.iter_mut().enumerate().for_each(|(idx, byte)| {
-                let j = (idx + 1) & 0xFF;
-                let value = self.box_data[j]
-                    .wrapping_add(self.box_data[(self.box_data[j] as usize + j) & 0xFF]);
-                *byte ^= self.box_data[value as usize & 0xFF];
+    pub fn apply_keystream(&self, mut data: Vec<u8>) -> Vec<u8> {
+        data.par_chunks_mut(CHUNK_SIZE)
+            .enumerate()
+            .for_each(|(chunk_idx, chunk)| {
+                let global_offset = chunk_idx * CHUNK_SIZE;
+                chunk.iter_mut().enumerate().for_each(|(local_idx, byte)| {
+                    let global_idx = global_offset + local_idx;
+                    let j = (global_idx + 1) & 0xFF;
+                    let value = self.box_data[j]
+                        .wrapping_add(self.box_data[(self.box_data[j] as usize + j) & 0xFF]);
+                    *byte ^= self.box_data[value as usize & 0xFF];
+                });
             });
-        });
 
-        output
+        data
     }
 }
